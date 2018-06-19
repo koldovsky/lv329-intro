c94e8f2a9bc1e0340f1b4582ef564686b347239e
koldovsky <koldovsky@yahoo.com>
2018-06-19 16:59:29 +0000

    Initial

---
+++ hello.py
@@ -0,0 +1,8 @@
+# user_name = input("User Name:")
+# msg = "Hello, " + user_name
+user_age = input("User Age: ")
+print("Your age is " + user_age)
+next_age = int(user_age) + 1
+print("Next year you will be: " + str(next_age))
+
+
